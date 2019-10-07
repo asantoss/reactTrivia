687700a8cbbe3178974e550eb30fcacec471c81e
Juan Castillo <castillojuan1000@gmail.com>
2019-10-06 23:06:16 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Landing from './components/pages/Landing';
 import Room from './components/pages/Room';
 
+
 function App() {
 	return (
 		<Router>
-			<div className='App'>
+			<div className='App' >
 				<NavBar />
-				<Route to='/room/:id' component={Room} />
-				<Route exact to='/' component={Landing} />
+				<Route path='/room/:id' component={Room} />
+				<Route exact path='/' component={Landing} />
 			</div>
 		</Router>
 	);
 }
 
 export default App;
+
