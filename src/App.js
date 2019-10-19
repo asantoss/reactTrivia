f9a448bf49da892bd0c8b200968197f8dd690a1c
Alexander Santos <alexsantosantana@live.com>
2019-10-19 11:08:58 -0400

    Working on merging

--- src/App.js
+++ src/App.js
@@ -6,14 +6,12 @@ import Landing from './components/containers/LandingContainer';
 import Room from './components/containers/RoomContainer';
 import Firebase, { FirebaseContext } from './components/firebase';
 import Authenticator from './components/containers/AuthenticatorContainer';
-import Homepage from './components/pages/Home';
 function App() {
 	return (
 		<FirebaseContext.Provider value={new Firebase()}>
 			<Router>
 				<div className='App'>
 					<NavBar />
-					<Route exact path='/home' component={Homepage} />
 					<Route exact path='/' component={Landing} />
 					<Route path='/rooms/:id' component={Room} />
 					<Route exact path='/signup' component={Authenticator} />
